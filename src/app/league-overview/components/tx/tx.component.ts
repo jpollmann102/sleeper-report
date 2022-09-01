d280f30fcdf0d442d36171ae4efb2024ec80e3d5
Josh Pollmann <joshpollmann102@gmail.com>
2022-08-31 23:43:17 -0400

    Add team pictures to transactions

--- src/app/league-overview/components/tx/tx.component.ts
+++ src/app/league-overview/components/tx/tx.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { LeagueUser } from 'src/app/interfaces/league-user';
 import { Transaction } from 'src/app/interfaces/transaction';
 import { LeagueService } from '../../services/league.service';
 
@@ -11,5 +12,10 @@ export class TxComponent {
   @Input() tx:Transaction | null = null;
 
   constructor(public leagueService:LeagueService) { }
+
+  getPicture(leagueUser:LeagueUser | undefined) {
+    if(leagueUser?.metadata.avatar) return leagueUser.metadata.avatar;
+    else return 'assets/football-helmet.png';
+  }
 
 }
